7db170406bdd8953cfa22a2977ac622c5c9a3895
Whitaker-Ralph <ralph@whitaker.kr>
2023-12-04 09:50:54 +0900

    Paint Color part One

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const color = document.getElementById("color");
 const lineWidth = document.getElementById("line-width");
 const canvas = document.querySelector("canvas");
 const ctx = canvas.getContext("2d");
@@ -6,6 +7,8 @@ canvas.width = 800;
 canvas.height = 800;
 
 ctx.lineWidth = lineWidth.value;
+
+let isPainting = false;
 
 const colors = [
   "#ff3838",
@@ -39,9 +42,16 @@ const onLineWidthChange = (event) => {
   ctx.lineWidth = event.target.value;
 };
 
+const onColorChange = (event) => {
+  ctx.strokeStyle = event.target.value;
+  ctx.fillStyle = event.target.value;
+};
+
 canvas.addEventListener("mousemove", onMove);
 canvas.addEventListener("mousedown", startPainting);
 canvas.addEventListener("mouseup", cancelPainting);
 canvas.addEventListener("mouseleave", cancelPainting);
 
 lineWidth.addEventListener("change", onLineWidthChange);
+
+color.addEventListener("change", onColorChange);
